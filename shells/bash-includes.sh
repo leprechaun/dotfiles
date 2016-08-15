477c5d838ee7a20a791ee7e9a349ad9035585550
Laurence MacGuire <leprechaun@gmail.com>
2016-08-15 12:10:26 +0800

    Changes everywhere ...
    terminals vs bce, vim.rc, beets etc ...

--- shells/bash-includes.sh
+++ shells/bash-includes.sh
@@ -9,11 +9,12 @@ PATH=$PATH:/home/leprechaun/.bin/
 GIT_PS1_SHOWDIRTYSTATE=1
 GIT_PS1_SHOWUNTRACKEDFILES=1
 PS1='\u@\h$(__git_ps1 " [\[\033[01;32m\]%s\[\033[00m\]]"):\w$ '
+PS1='[\W\[$(tput sgr0)$(__git_ps1 "@\033[01;32m\]%s\033[00m")] \\$ '
 
 
 # Terminal stuffs
-alias screen='screen -T screen-256color'
-export TERM=xterm-256color
+#alias screen='screen -T screen-256color'
+#export TERM=xterm-256color
 
 export LANGUAGE=en_CA.UTF-8
 export LANG=en_CA.UTF-8
@@ -24,3 +25,9 @@ export EDITOR=vim
 # Custom completions
 complete -C aws_completer aws
 source ~/.dotfiles/tmux/tmux-completion.sh
+
+source /home/leprechaun/.dotfiles/shells/rea-saml-aws.sh
+
+alias uzbl=uzbl-tabbed
+
+export MANPAGER="/bin/sh -c \"col -b | vim -c 'set ft=man ts=8 nomod noma' -\""
